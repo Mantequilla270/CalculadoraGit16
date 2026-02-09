5205c6d95769bedb20fa9ca00f26d811fdb7f417
Javier <javier.ortiz07@somo.eus>
2026-02-09 13:34:21 +0100

    Cambio en dividir: manejo de error sin excepción

--- src/calculadora16/Calculadora16.java
+++ src/calculadora16/Calculadora16.java
@@ -15,10 +15,12 @@ public class Calculadora16 {
 	
 	public double dividir(int a, int b) {
 		 if (b == 0) {
-			 throw new IllegalArgumentException("No se puede dividir entre cero");
+		 System.out.println("Error: división entre cero");
+		 return 0;
 		 }
-		 return (double) (a / b);
+		 return (double) a / b;
 	}
+
 
 
 }
